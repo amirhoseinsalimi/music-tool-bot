5df71e692436eb91aff21cdb46892e357491d877
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-01 11:30:38 +0330

    Write a dummy bot

---
+++ bot.py
@@ -0,0 +1,29 @@
+# Built-in modules
+import os
+import json
+import env
+
+# Third-party modules
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# My modules
+from redisconfig import persistence
+
+# Global variables
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+print(BOT_TOKEN)
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    print(json.dumps(update, sort_keys=True, indent=4, default=str))
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+updater = Updater(os.getenv("BOT_TOKEN"), persistence=persistence)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
