f08c4491652bac48a5569cd9b9fd1a6360d99034
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-01 11:30:18 +0330

    Create a redis connection exporter module

---
+++ redisconfig.py
@@ -0,0 +1,15 @@
+import os
+import redis
+import env
+from redispersistence.persistence import RedisPersistence
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
+    db=1,
+    client_name=os.getenv("APP_NAME"),
+    password=os.getenv("REDIS_PASSWORD")
+)
+persistence = RedisPersistence(redis_client)
+
+print(os.getenv("REDIS_PORT"))
