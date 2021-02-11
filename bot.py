46dcbe9dab80004d4ed17b7afc8a5c2b308d322d
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-11 11:37:00 +0330

    Move `download_function` to a separate module

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ import env
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, Filters, MessageHandler
 import requests
+from downloader import download_file
+import music_tag
 
 ############################
 # My modules ###############
@@ -78,30 +80,6 @@ def create_user_directory(user_id: int) -> str:
         raise Exception(f"Can't create directory for user_id: {user_id}")
 
     return user_download_dir
-
-
-def download_file(user_id: int, file_to_download, file_type: str, context: CallbackContext) -> str:
-    user_download_dir = f"downloads/{user_id}"
-    file_id = ''
-    file_extension = ''
-
-    if file_type == 'audio':
-        file_id = context.bot.get_file(file_to_download.file_id)
-        file_name = file_to_download.file_name
-        file_extension = file_name.split(".")[-1]
-    elif file_type == 'photo':
-        file_id = context.bot.get_file(file_to_download.file_id)
-        file_extension = 'jpg'
-
-    file_download_path = f"{user_download_dir}/{file_id.file_id}.{file_extension}"
-
-    try:
-        file_id.download(f"{user_download_dir}/{file_id.file_id}.{file_extension}")
-    except:
-        file_download_path = None
-        raise Exception(f"Couldn't download the file with file_id: {file_id}")
-
-    return file_download_path
 
 
 def handle_music_message(update: Update, context: CallbackContext) -> None:
