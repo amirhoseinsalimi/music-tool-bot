d675d53aaa495f381ee0728895d0a5c6fa2e32be
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-13 22:38:59 +0330

    Change "APP_NAME" to "BOT_NAME" + Add "BOT_USERNAME"

--- redisconfig.py
+++ redisconfig.py
@@ -7,7 +7,7 @@ redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=os.getenv("REDIS_PORT"),
     db=1,
-    client_name=os.getenv("APP_NAME"),
+    client_name=os.getenv("BOT_NAME"),
     password=os.getenv("REDIS_PASSWORD")
 )
 persistence = RedisPersistence(redis_client)
