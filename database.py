34a62d7fbfc7d1efbe9ff82c500abbb485994d00
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-20 16:06:40 +0330

    Write dummy code with SQLAlchemy

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 import os
 import env
 import mysql.connector
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import time
 
 DB_HOST = os.getenv("DB_HOST") if os.getenv("DB_HOST") else 'localhost'
 DB_PORT = int(os.getenv("DB_PORT")) if int(os.getenv("DB_PORT")) else 3306
@@ -8,22 +12,60 @@ DB_USERNAME = os.getenv("DB_USERNAME") if os.getenv("DB_USERNAME") else ''
 DB_PASSWORD = os.getenv("DB_PASSWORD") if os.getenv("DB_PASSWORD") else ''
 DB_NAME = os.getenv("DB_NAME") if os.getenv("DB_NAME") else ''
 
-OWNER_USER_ID = os.getenv("ADMIN_USER_ID") if os.getenv("OWNER_USER_ID") else ''
+OWNER_USER_ID = os.getenv("ADMIN_USER_ID") if os.getenv("OWNER_USER_ID") else 0
+OWNER_USER_ID = int(OWNER_USER_ID)
+print(type(OWNER_USER_ID))
 
-connection = mysql.connector.connect(
-    host=DB_HOST,
-    port=DB_PORT,
-    user=DB_USERNAME,
-    password=DB_PASSWORD,
-    database=DB_NAME
+CONNECTION_STRING = f"mysql://{DB_USERNAME}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(
+    CONNECTION_STRING,
+    pool_size=20,
+    dialect='mysql',
+    echo=True
 )
 
-cursor = connection.cursor(buffered=True)
+MetaData().create_all(engine)
 
-cursor.execute("CREATE TABLE IF NOT EXISTS `users` "
-               "(id INT AUTO_INCREMENT PRIMARY KEY, user_id INT UNIQUE, number_of_files_sent INT DEFAULT 0)")
-cursor.execute("CREATE TABLE IF NOT EXISTS `admins` "
-               "(id INT AUTO_INCREMENT PRIMARY KEY, user_id INT UNIQUE, is_owner BOOLEAN DEFAULT false)")
+Session = sessionmaker(bind=engine)
+session = Session()
 
-if OWNER_USER_ID:
-    cursor.execute(f"INSERT IGNORE INTO `admins` (`user_id`, `is_owner`) VALUES ({OWNER_USER_ID}, true)")
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column('id', Integer, primary_key=True, unique=True, nullable=False)
+    user_id = Column('user_id', Integer, unique=True, nullable=False)
+    number_of_files_sent = Column('number_of_files_sent', Integer, default=0, nullable=False)
+
+    def __init__(self, user_id: int, number_of_files_sent: int = 0):
+        self.user_id = user_id
+        self.number_of_files_sent = number_of_files_sent
+
+
+class Admin(Base):
+    __tablename__ = 'admins'
+
+    id = Column('id', Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    admin_id = Column('admin_id', Integer, unique=True, nullable=False)
+
+    def __init__(self, admin_id):
+        self.admin_id = admin_id
+
+
+owner = session.query(Admin) \
+    .filter(Admin.admin_id == OWNER_USER_ID) \
+    .first()
+
+if owner:
+    print('A')
+    print(owner)
+else:
+    print('B')
+    admin = Admin(OWNER_USER_ID)
+    session.add(admin)
+
+session.commit()
+session.close()
