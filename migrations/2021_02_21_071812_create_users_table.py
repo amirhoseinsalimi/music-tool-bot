01245b59cf8c580ad79b2fb8607431ed521b07b1
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-21 10:57:04 +0330

    Create migrations for users and admins

---
+++ migrations/2021_02_21_071812_create_users_table.py
@@ -0,0 +1,15 @@
+from orator.migrations import Migration
+
+
+class CreateUsersTable(Migration):
+
+    def up(self):
+        with self.schema.create('users') as table:
+            table.increments('id')
+            table.integer('user_id').unique()
+            table.integer('number_of_files_sent').default(0)
+
+            table.timestamps()
+
+    def down(self):
+        self.schema.drop('users')
