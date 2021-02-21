0c14a49f8ece9eb8cef8beb6043cb3a64196b71e
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-21 19:32:31 +0330

    Also `new_art_path` inside `reset_context_user_data`

--- bot.py
+++ bot.py
@@ -176,6 +176,7 @@ def reset_context_user_data(context: CallbackContext) -> None:
     user_data['music_path'] = ''
     user_data['music_duration'] = ''
     user_data['art_path'] = ''
+    user_data['new_art_path'] = ''
     user_data['current_active_module'] = ''
     user_data['music_message_id'] = ''
 
