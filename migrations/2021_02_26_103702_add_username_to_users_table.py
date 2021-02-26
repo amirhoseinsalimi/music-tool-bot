1aef0b4a701bf9c61813c5e696579cb48b474820
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-02-26 14:58:59 +0330

    Make `username` field nullable

--- migrations/2021_02_26_103702_add_username_to_users_table.py
+++ migrations/2021_02_26_103702_add_username_to_users_table.py
@@ -4,7 +4,7 @@ from orator.migrations import Migration
 class AddUsernameToUsersTable(Migration):
     def up(self):
         with self.schema.table('users') as table:
-            table.string('username').after('user_id').default('')
+            table.string('username').after('user_id').default('').nullable()
 
     def down(self):
         with self.schema.table('users') as table:
