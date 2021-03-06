c1a625c42a6582b3a1b52df8e46e6d4364e564da
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-03-06 13:28:12 +0330

    Use a `dict` literal to reset `user_data`

--- utils/__init__.py
+++ utils/__init__.py
@@ -105,6 +105,7 @@ def is_user_owner(user_id: int) -> bool:
 
 def reset_user_data_context(context: CallbackContext) -> None:
     user_data = context.user_data
+    language = user_data['language'] if ('language' in user_data) else 'en'
 
     if 'music_path' in user_data:
         delete_file(user_data['music_path'])
@@ -113,14 +114,17 @@ def reset_user_data_context(context: CallbackContext) -> None:
     if 'new_art_path' in user_data:
         delete_file(user_data['new_art_path'])
 
-    user_data['tag_editor'] = {}
-    user_data['music_path'] = ''
-    user_data['music_duration'] = ''
-    user_data['art_path'] = ''
-    user_data['new_art_path'] = ''
-    user_data['current_active_module'] = ''
-    user_data['music_message_id'] = ''
-    user_data['language'] = user_data['language'] if ('language' in user_data) else 'en'
+    new_user_data = {
+        'tag_editor': {},
+        'music_path': '',
+        'music_duration': 0,
+        'art_path': '',
+        'new_art_path': '',
+        'current_active_module': '',
+        'music_message_id': 0,
+        'language': language,
+    }
+    context.user_data.update(new_user_data)
 
 
 def save_text_into_tag(value: str, current_tag: str, context: CallbackContext, is_number: bool = False) -> None:
