a488a451b1a842ffd013f9d2a2b1593408e182d7
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2021-04-08 17:27:30 +0430

    Use constants instead of string for search in keys

--- utils/lang.py
+++ utils/lang.py
@@ -1,24 +1,75 @@
+START_MESSAGE = "START_MESSAGE"
+START_OVER_MESSAGE = "START_OVER_MESSAGE"
+HELP_MESSAGE = "HELP_MESSAGE"
+ABOUT_MESSAGE = "ABOUT_MESSAGE"
+DEFAULT_MESSAGE = "DEFAULT_MESSAGE"
+ASK_WHICH_MODULE = "ASK_WHICH_MODULE"
+ASK_WHICH_TAG = "ASK_WHICH_TAG"
+ASK_FOR_ALBUM = "ASK_FOR_ALBUM"
+ASK_FOR_ARTIST = "ASK_FOR_ARTIST"
+ASK_FOR_TITLE = "ASK_FOR_TITLE"
+ASK_FOR_GENRE = "ASK_FOR_GENRE"
+ASK_FOR_YEAR = "ASK_FOR_YEAR"
+ASK_FOR_ALBUM_ART = "ASK_FOR_ALBUM_ART"
+ASK_FOR_DISK_NUMBER = "ASK_FOR_DISK_NUMBER"
+ASK_FOR_TRACK_NUMBER = "ASK_FOR_TRACK_NUMBER"
+ALBUM_ART_CHANGED = "ALBUM_ART_CHANGED"
+EXPECTED_NUMBER_MESSAGE = "EXPECTED_NUMBER_MESSAGE"
+CLICK_PREVIEW_MESSAGE = "CLICK_PREVIEW_MESSAGE"
+CLICK_DONE_MESSAGE = "CLICK_DONE_MESSAGE"
+LANGUAGE_CHANGED = "LANGUAGE_CHANGED"
+MUSIC_LENGTH = "MUSIC_LENGTH"
+REPORT_BUG_MESSAGE = "REPORT_BUG_MESSAGE"
+ERR_CREATING_USER_FOLDER = "ERR_CREATING_USER_FOLDER"
+ERR_ON_DOWNLOAD_AUDIO_MESSAGE = "ERR_ON_DOWNLOAD_AUDIO_MESSAGE"
+ERR_ON_DOWNLOAD_PHOTO_MESSAGE = "ERR_ON_DOWNLOAD_PHOTO_MESSAGE"
+ERR_TOO_LARGE_FILE = "ERR_TOO_LARGE_FILE"
+ERR_ON_READING_TAGS = "ERR_ON_READING_TAGS"
+ERR_ON_UPDATING_TAGS = "ERR_ON_UPDATING_TAGS"
+ERR_ON_UPLOADING = "ERR_ON_UPLOADING"
+ERR_NOT_IMPLEMENTED = "ERR_NOT_IMPLEMENTED"
+ERR_OUT_OF_RANGE = "ERR_OUT_OF_RANGE"
+ERR_MALFORMED_RANGE = "ERR_MALFORMED_RANGE"
+ERR_BEGINNING_POINT_IS_GREATER = "ERR_BEGINNING_POINT_IS_GREATER"
+BTN_TAG_EDITOR = "BTN_TAG_EDITOR"
+BTN_MUSIC_TO_VOICE_CONVERTER = "BTN_MUSIC_TO_VOICE_CONVERTER"
+BTN_MUSIC_CUTTER = "BTN_MUSIC_CUTTER"
+BTN_BITRATE_CHANGER = "BTN_BITRATE_CHANGER"
+BTN_ARTIST = "BTN_ARTIST"
+BTN_TITLE = "BTN_TITLE"
+BTN_ALBUM = "BTN_ALBUM"
+BTN_GENRE = "BTN_GENRE"
+BTN_YEAR = "BTN_YEAR"
+BTN_ALBUM_ART = "BTN_ALBUM_ART"
+BTN_DISK_NUMBER = "BTN_DISK_NUMBER"
+BTN_TRACK_NUMBER = "BTN_TRACK_NUMBER"
+BTN_BACK = "BTN_BACK"
+BTN_NEW_FILE = "BTN_NEW_FILE"
+MUSIC_CUTTER_HELP = "MUSIC_CUTTER_HELP"
+DONE = "DONE"
+OR = "OR"
+
 REPORT_BUG_MESSAGE_EN = "That's my fault! Please send a bug report here: @amirhoseinsalimii"
 REPORT_BUG_MESSAGE_FA = "این اشتباه منه! لطفا این باگ رو از اینجا گزارش کنید: @amirhoseinsalimii"
 EG_EN = "e.g."
 EG_FA = "مثل"
 
 keys = {
-    "START_MESSAGE": {
+    START_MESSAGE: {
         "en": "Hello there! 👋\n"
               "Let's get started. Just send me a music and see how awesome I am!",
         "fa": "سلام! 👋\n"
               "خب شروع کنیم. یه موزیک برام بفرست تا ببینی چقدر خفنم!",
     },
-    "START_OVER_MESSAGE": {
+    START_OVER_MESSAGE: {
         "en": "Send me a music and see how awesome I am!",
         "fa": "یه موزیک برام بفرست تا ببینی چقدر خفنم!",
     },
-    "HELP_MESSAGE": {
+    HELP_MESSAGE: {
         "en": "It's simple! Just send or forward me an audio track, an MP3 file or a music. I'm waiting... 😁",
         "fa": "ساده س! یه فایل صوتی، یه MP3 یا یه موزیک برام بفرست. منتظرم... 😁",
     },
-    "ABOUT_MESSAGE": {
+    ABOUT_MESSAGE: {
         "en": "This bot is created by @amirhoseinsalimii in Python language.\n"
               "The source code of this project is available on"
               " [GitHub](https://github.com/amirhoseinsalimi/music-tool-bot).\n\n"
@@ -31,180 +82,180 @@ keys = {
               "اگر سوال یا بازخوردی دارید میتونید در تلگرام بهم پیام بدید. یا اگر برنامه نویس هستید و ایده "
               "ای برای بهتر کردن این ربات دارید، از PR هاتون استقبال میکنم."
     },
-    "DEFAULT_MESSAGE": {
+    DEFAULT_MESSAGE: {
         "en": "Send or forward me an audio track, an MP3 file or a music. I'm waiting... 😁",
         "fa": "یه فایل صوتی، یه MP3 یا یه موزیک برام بفرست... منتظرم... 😁",
     },
-    "ASK_WHICH_MODULE": {
+    ASK_WHICH_MODULE: {
         "en": "What do you want to do with this file?",
         "fa": "میخوای با این فایل چیکار کنی؟",
     },
-    "ASK_WHICH_TAG": {
+    ASK_WHICH_TAG: {
         "en": "Which tag do you want to edit?",
         "fa": "چه تگی رو میخوای ویرایش کنی؟",
     },
-    "ASK_FOR_ALBUM": {
+    ASK_FOR_ALBUM: {
         "en": "Enter the name of the album:",
         "fa": "نام آلبوم را وارد کنید:",
     },
-    "ASK_FOR_ARTIST": {
+    ASK_FOR_ARTIST: {
         "en": "Enter the name of the artist:",
         "fa": "نام خواننده رو وارد کنید:",
     },
-    "ASK_FOR_TITLE": {
+    ASK_FOR_TITLE: {
         "en": "Enter the title:",
         "fa": "عنوان رو وارد کنید:",
     },
-    "ASK_FOR_GENRE": {
+    ASK_FOR_GENRE: {
         "en": "Enter the genre:",
         "fa": "ژانر رو وارد کنید:",
     },
-    "ASK_FOR_YEAR": {
+    ASK_FOR_YEAR: {
         "en": "Enter the publish year:",
         "fa": "سال انتشار رو وارد کنید:",
     },
-    "ASK_FOR_ALBUM_ART": {
+    ASK_FOR_ALBUM_ART: {
         "en": "Send me a photo:",
         "fa": "یک عکس برام بفرست:",
     },
-    "ASK_FOR_DISK_NUMBER": {
+    ASK_FOR_DISK_NUMBER: {
         "en": "Enter the disk number:",
         "fa": "شماره دیسک را وارد کنید:",
     },
-    "ASK_FOR_TRACK_NUMBER": {
+    ASK_FOR_TRACK_NUMBER: {
         "en": "Enter the track number:",
         "fa": "شماره ترک را وارد کنید:",
     },
-    "ALBUM_ART_CHANGED": {
+    ALBUM_ART_CHANGED: {
         "en": "Album art changed",
         "fa": "عکس آلبوم تغییر یافت.",
     },
-    "EXPECTED_NUMBER_MESSAGE": {
+    EXPECTED_NUMBER_MESSAGE: {
         "en": "You entered a string instead of a number. Although this is not a problem, "
               "I guess you entered this input by mistake.",
         "fa": "شما یک متن رو به جای عدد وارد کردید. اگر چه اشکالی نداره ولی حدس میزنم"
               " اشتباهی وارد کردی."},
-    "CLICK_PREVIEW_MESSAGE": {
+    CLICK_PREVIEW_MESSAGE: {
         "en": "If you want to preview your changes click /preview.",
         "fa": "اگر میخوای تغییرات رو تا الان ببینی از دستور /preview استفاده کن.",
     },
-    "CLICK_DONE_MESSAGE": {
+    CLICK_DONE_MESSAGE: {
         "en": "Click /done to save your changes.",
         "fa": "روی /done کلیک کن تا تغییراتت ذخیره بشن.",
     },
-    "LANGUAGE_CHANGED": {
+    LANGUAGE_CHANGED: {
         "en": "Language has been changed. If you want to change the language later, use /language command.",
         "fa": "زبان تغییر یافت. اگر میخواهید زبان را مجددا تغییر دهید، از دستور /language استفاده کنید.",
     },
-    "MUSIC_LENGTH": {
+    MUSIC_LENGTH: {
         "en": "The file length is {}.",
         "fa": "طول کل فایل {} است.",
     },
-    "REPORT_BUG_MESSAGE": {
+    REPORT_BUG_MESSAGE: {
         "en": "That's my fault! Please send a bug report here: @amirhoseinsalimii",
         "fa": "این اشتباه منه! لطفا این باگ رو از اینجا گزارش کنید: @amirhoseinsalimii",
     },
-    "ERR_CREATING_USER_FOLDER": {
+    ERR_CREATING_USER_FOLDER: {
         "en": f"Error on starting... {REPORT_BUG_MESSAGE_EN}",
         "fa": f"به مشکل خوردم... {REPORT_BUG_MESSAGE_FA}",
     },
-    "ERR_ON_DOWNLOAD_AUDIO_MESSAGE": {
+    ERR_ON_DOWNLOAD_AUDIO_MESSAGE: {
         "en": f"Sorry, I couldn't download your file... {REPORT_BUG_MESSAGE_EN}",
         "fa": f"متاسفم، نتونستم فایلت رو دانلود کنم... {REPORT_BUG_MESSAGE_FA}",
     },
-    "ERR_ON_DOWNLOAD_PHOTO_MESSAGE": {
+    ERR_ON_DOWNLOAD_PHOTO_MESSAGE: {
         "en": f"Sorry, I couldn't download your file... {REPORT_BUG_MESSAGE_EN}",
         "fa": f"متاسفم، نتونستم فایلت رو دانلود کنم... {REPORT_BUG_MESSAGE_FA}",
     },
-    "ERR_TOO_LARGE_FILE": {
+    ERR_TOO_LARGE_FILE: {
         "en": "This file is too big that I can process, sorry!",
         "fa": "این فایل بزرگتر از چیزی هست که من بتونم پردازش کنم، شرمنده!",
     },
-    "ERR_ON_READING_TAGS": {
+    ERR_ON_READING_TAGS: {
         "en": f"Sorry, I couldn't read the tags of the file... {REPORT_BUG_MESSAGE_EN}",
         "fa": f"متاسفم، نتونستم تگ های فایل رو بخونم... {REPORT_BUG_MESSAGE_FA}",
     },
-    "ERR_ON_UPDATING_TAGS": {
+    ERR_ON_UPDATING_TAGS: {
         "en": f"Sorry, I couldn't update tags the tags of the file... {REPORT_BUG_MESSAGE_EN}",
         "fa": f"متاسفم، نتونستم تگ های فایل رو آپدیت کنم... {REPORT_BUG_MESSAGE_FA}",
     },
-    "ERR_ON_UPLOADING": {
+    ERR_ON_UPLOADING: {
         "en": "Sorry, due to network issues, I couldn't upload your file. Please try again.",
         "fa": "متاسفم. به دلیل اشکالات شبکه نتونستم فایل رو آپلود کنم. لطفا دوباره امتحان کن.",
     },
-    "ERR_NOT_IMPLEMENTED": {
+    ERR_NOT_IMPLEMENTED: {
         "en": "This feature has not been implemented yet. Sorry!",
         "fa": "این قابلیت هنوز پیاده سازی نشده. شرمنده!",
     },
-    "ERR_OUT_OF_RANGE": {
+    ERR_OUT_OF_RANGE: {
         "en": "The range you entered is out of the actual file duration. The file length is {}.",
         "fa": "بازه ای که انتخاب کردید خارج از طول کل فایل هست. طول کل فایل {} است.",
     },
-    "ERR_MALFORMED_RANGE": {
+    ERR_MALFORMED_RANGE: {
         "en": "You have entered a malformed pattern. Please try again. {}",
         "fa": "شما یک الگوی اشتباه وارد کردید. لطفا دوباره امتحان کنید. {}",
     },
-    "ERR_BEGINNING_POINT_IS_GREATER": {
+    ERR_BEGINNING_POINT_IS_GREATER: {
         "en": "The ending point should be greater than starting point",
         "fa": "زمان پایان باید از زمان شروع بزرگتر باشد.",
     },
-    "BTN_TAG_EDITOR": {
+    BTN_TAG_EDITOR: {
         "en": "🎵 Tag Editor",
         "fa": "🎵 تغییر تگ ها",
     },
-    "BTN_MUSIC_TO_VOICE_CONVERTER": {
+    BTN_MUSIC_TO_VOICE_CONVERTER: {
         "en": "🗣 Music to Voice Converter",
         "fa": "🗣 تبدیل به پیام صوتی",
     },
-    "BTN_MUSIC_CUTTER": {
+    BTN_MUSIC_CUTTER: {
         "en": "✂️ Music Cutter",
         "fa": "✂️ بریدن آهنگ",
     },
-    "BTN_BITRATE_CHANGER": {
+    BTN_BITRATE_CHANGER: {
         "en": "🎙 Bitrate Changer",
         "fa": "🎙 تغییر بیت ریت",
     },
-    "BTN_ARTIST": {
+    BTN_ARTIST: {
         "en": "🗣 Artist",
         "fa": "🗣 خواننده",
     },
-    "BTN_TITLE": {
+    BTN_TITLE: {
         "en": "🎵 Title",
         "fa": "🎵 عنوان",
     },
-    "BTN_ALBUM": {
+    BTN_ALBUM: {
         "en": "🎼 Album",
         "fa": "🎼 آلبوم",
     },
-    "BTN_GENRE": {
+    BTN_GENRE: {
         "en": "🎹 Genre",
         "fa": "🎹 ژانر",
     },
-    "BTN_YEAR": {
+    BTN_YEAR: {
         "en": "📅 Year",
         "fa": "📅 سال",
     },
-    "BTN_ALBUM_ART": {
+    BTN_ALBUM_ART: {
         "en": "🖼 Album Art",
         "fa": "🖼 عکس آلبوم",
     },
-    "BTN_DISK_NUMBER": {
+    BTN_DISK_NUMBER: {
         "en": "💿 Disk Number",
         "fa": "💿  شماره دیسک",
     },
-    "BTN_TRACK_NUMBER": {
+    BTN_TRACK_NUMBER: {
         "en": "▶️ Track Number",
         "fa": "▶️ شماره ترک",
     },
-    "BTN_BACK": {
+    BTN_BACK: {
         "en": "🔙 Back",
         "fa": "🔙 بازگشت",
     },
-    "BTN_NEW_FILE": {
+    BTN_NEW_FILE: {
         "en": "🆕 New File",
         "fa": "🆕 فایل جدید",
     },
-    "MUSIC_CUTTER_HELP": {
+    MUSIC_CUTTER_HELP: {
         "en": "\n\nNow send me which part of the music you want to cut out?\n"
               "The file length is {}.\n\n"
               "Valid patterns are:\n"
@@ -224,11 +275,11 @@ keys = {
               "- فاصله های اضافی در نظر گرفته نمیشن\n"
               "- تنها اعداد انگلیسی مجاز هستند",
     },
-    "DONE": {
+    DONE: {
         "en": "Done!",
         "fa": "انجام شد!",
     },
-    "OR": {
+    OR: {
         "en": "or",
         "fa": "یا",
     },
