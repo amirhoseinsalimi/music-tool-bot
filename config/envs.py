9d6455014a21a039d40ff90400963fb6cd74ec20
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2023-08-28 09:15:53 +0330

    Use SQLite instead of MySQL

--- config/envs.py
+++ config/envs.py
@@ -10,11 +10,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 OWNER_USER_ID = os.getenv("OWNER_USER_ID") if os.getenv("OWNER_USER_ID") else 0
 OWNER_USER_ID_INT = int(OWNER_USER_ID)
 
-DB_HOST = os.getenv("DB_HOST") if os.getenv("DB_HOST") else 'localhost'
-DB_PORT = int(os.getenv("DB_PORT")) if int(os.getenv("DB_PORT")) else 3306
 DB_NAME = os.getenv("DB_NAME") if os.getenv("DB_NAME") else ''
-DB_USERNAME = os.getenv("DB_USERNAME") if os.getenv("DB_USERNAME") else ''
-DB_PASSWORD = os.getenv("DB_PASSWORD") if os.getenv("DB_PASSWORD") else ''
 
 BTC_WALLET_ADDRESS = os.getenv("BTC_WALLET_ADDRESS") if os.getenv("BTC_WALLET_ADDRESS") else ''
 ETH_WALLET_ADDRESS = os.getenv("ETH_WALLET_ADDRESS") if os.getenv("ETH_WALLET_ADDRESS") else ''
