fff548d04141f140b97e2407a81719d00d615a3f
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2025-03-15 12:29:07 +0330

    feature(admin): implement message broadcasting

--- config/telegram_bot.py
+++ config/telegram_bot.py
@@ -1,10 +1,13 @@
-from telegram.ext import Application, Defaults
 from telegram.constants import ParseMode
+from telegram.ext import Application, Defaults
 
 from config.envs import BOT_TOKEN
 
 defaults = Defaults(parse_mode=ParseMode.MARKDOWN_V2)
 
-app = Application.builder().token(BOT_TOKEN).build()
+app = Application.builder() \
+    .token(BOT_TOKEN) \
+    .concurrent_updates(False) \
+    .build()
 
 add_handler = app.add_handler
