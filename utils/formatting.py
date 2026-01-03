048aaabecb2afe9cf818a01d5c281089dc089665
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2026-01-03 15:49:10 +0330

    fix(cutter): fix the leading zero issue with file duration

--- utils/formatting.py
+++ utils/formatting.py
@@ -42,9 +42,9 @@ def convert_seconds_to_human_readable_form(seconds: int) -> str:
         return "00:00"
 
     minutes = int(seconds / 60)
-    remainder = seconds % 60
+    remainder = int(seconds % 60)
 
     minutes_formatted = str(minutes) if minutes >= 10 else "0" + str(minutes)
     seconds_formatted = str(remainder) if remainder >= 10 else "0" + str(remainder)
 
-    return f"{minutes_formatted}:{int(float(seconds_formatted))}"
+    return f"{minutes_formatted}:{seconds_formatted}"
