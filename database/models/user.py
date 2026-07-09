4a1e7c5c0dc3bddd18b1550ce37eee49252f29a9
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2026-07-09 12:29:04 +0330

    feat: add language_id foreign key to users

--- database/models/user.py
+++ database/models/user.py
@@ -3,10 +3,23 @@ from masoniteorm.relationships import belongs_to
 
 
 class User(Model):
-    __fillable__ = ['user_id', 'username', 'language', 'number_of_files_sent', 'user_status_id', 'last_interaction_at']
+    __fillable__ = [
+        'user_id',
+        'username',
+        'language_id',
+        'number_of_files_sent',
+        'user_status_id',
+        'last_interaction_at'
+    ]
 
     @belongs_to('user_status_id', 'id')
     def user_status(self):
         from database.models.user_status import UserStatus
 
         return UserStatus
+
+    @belongs_to('language_id', 'id')
+    def language(self):
+        from database.models.language import Language
+
+        return Language
