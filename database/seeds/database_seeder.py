f88a8ce00f0aea8b6b8638a2cfc333fe44d25577
Amir Hosein Salimi <ahosein.salimi@gmail.com>
2026-07-09 12:28:56 +0330

    feat: add languages table and seed the six supported languages

--- database/seeds/database_seeder.py
+++ database/seeds/database_seeder.py
@@ -1,9 +1,10 @@
 from masoniteorm.seeds import Seeder
 
-from database.seeds.user_status_table_seeder import UserStatusTableSeeder
+from database.seeds.language_table_seeder import LanguageTableSeeder
 from database.seeds.owner_table_seeder import OwnerTableSeeder
+from database.seeds.user_status_table_seeder import UserStatusTableSeeder
 
 
 class DatabaseSeeder(Seeder):
     def run(self):
-        self.call(UserStatusTableSeeder, OwnerTableSeeder)
+        self.call(LanguageTableSeeder, UserStatusTableSeeder, OwnerTableSeeder)
